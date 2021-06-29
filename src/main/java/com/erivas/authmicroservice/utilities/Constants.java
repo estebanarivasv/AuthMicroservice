f4a0acabc667dd09622bd74b66832622e70120c9
Esteban <estebanarivasv@gmail.com>
2021-06-29 14:13:05 -0300

    Add url constants

---
+++ src/main/java/com/erivas/authmicroservice/utilities/Constants.java
@@ -0,0 +1,10 @@
+package com.erivas.authmicroservice.utilities;
+
+public final class Constants {
+    public static final String LOGIN_URL = "/login";
+    public static final String REGISTER_URL = "/register";
+
+    private Constants() {
+
+    }
+}
